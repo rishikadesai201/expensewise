ed4a4879264950759db6ac95796eb38a7090bc25
VanshikaDoshi8 <vanshikadoshi2005@gmail.com>
2025-04-14 12:56:34 +0530

    transaction

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '12345678',
+  password: '1234',
   database: 'expensewise',
   waitForConnections: true,
   connectionLimit: 10,
